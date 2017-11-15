1180ded64f1e22481431c88be1eb548b97742492
BlackVirusScript <blackvirusscript@tutanota.com>
2017-11-15 19:30:36 -0200

    Create setup.py script.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import glob, os
+from distutils.core import setup
+
+PACKAGE_NAME = "qreader"
+
+setup(name=PACKAGE_NAME,
+      version="0.1",
+      description="A pure python reader for QR codes.",
+      url="https://github.com/ewino/qreader/",
+      author="Ehud Winograd",
+      maintainer="Ehud Winograd",
+      long_description='A pure python reader for QR codes. Made to be compatible to Python 2.7 to 3.5+.',
+      platforms=["Unix", "Windows"],
+      packages=['qreader'],
+      data_files=[(os.path.join('share', 'doc', PACKAGE_NAME), ['README.md'] + glob.glob('doc/*')),
+                  (os.path.join('share', 'doc', PACKAGE_NAME, 'tests'),glob.glob('tests/*.py')),
+                  (os.path.join('share', 'doc', PACKAGE_NAME, 'tests', 'resources', 'decoder'),glob.glob('tests/resources/decoder/*')),
+                  (os.path.join('share', 'doc', PACKAGE_NAME, 'tests', 'resources', 'scanner'),glob.glob('tests/resources/scanner/*'))],
+      requires=['Pillow', 'six', 'python_dateutil', 'reedsolo'])
